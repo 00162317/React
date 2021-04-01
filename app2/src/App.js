3ece925f003671da865d5d0f79c40a277e651139
Roberto Hernandez <00162317@uca.edu.sv>
2021-04-01 17:54:34 -0600

    Hola mundo

--- app2/src/App.js
+++ app2/src/App.js
@@ -1,11 +1,19 @@
-
 function App() {
+
+  const name = 'Roberto'; 
+  const x = true;
+
   return (
     <div className="App">
-       
-       
+      <h1>Hello from React, I'm {name}</h1>
+      
+      <h2>Haciendo una suma de 1 + 4 = {1+4}</h2>
+      <div>
+        
+        <h3>Haciendo un if: {x ? 'Si es true' : 'Es false'}</h3>
+
+      </div>
     </div>
   );
 }
-
 export default App;
