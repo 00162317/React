552be64dcfd8046d90b3490dbd9ed6084063296c
Roberto Hernandez <00162317@uca.edu.sv>
2021-04-09 14:51:46 -0600

    Creación de tu primer componente con REACT

--- linkedin/mi-hero/src/index.js
+++ linkedin/mi-hero/src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {render} from 'react-dom'//Para poder mostrar todos los contenidos en mi pantalla
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class HolaMundo extends React.Component{
+  render(){
+    return <p>Hola mundo</p>
+  }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+//Llamando a la clase hola mundo
+render(<HolaMundo/>,document.querySelector('#root')); 
