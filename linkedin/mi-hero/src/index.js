23171802910053955a31cbe3c9990a87ff1a1271
Roberto Hernandez <00162317@uca.edu.sv>
2021-04-09 14:57:03 -0600

    Organización e importación de componentes

--- linkedin/mi-hero/src/index.js
+++ linkedin/mi-hero/src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import {render} from 'react-dom'//Para poder mostrar todos los contenidos en mi pantalla
+import registerServiceWorker from './registerServiceWorker';
 
-class HolaMundo extends React.Component{
-  render(){
-    return <p>Hola mundo</p>
-  }
-}
+import HolaMundo from './components/HolaMundo';
 
 
 //Llamando a la clase hola mundo
