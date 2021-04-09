50574f6341f0ef287bd68f3e4f83a021dbd7bce4
Roberto Hernandez <00162317@uca.edu.sv>
2021-04-09 15:00:56 -0600

    Generación de código HTML con JSX

--- linkedin/mi-hero/src/components/HolaMundo.js
+++ linkedin/mi-hero/src/components/HolaMundo.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-class HolaMundo extends React.Component{
-    render(){
-      return <p>Hola mundo</p>
+class HolaMundo extends React.Component {
+    render() {
+        //Es importante siempre guardar mas de dos elementos en un div
+        //Y siempre cerrar las etiquteas con "/"
+        return (
+            <div>
+                <p>Hola mundo</p>
+                <p>Hola mundo</p>
+                <br/>
+            </div>
+        )
     }
-  }
+}
 
 export default HolaMundo;
