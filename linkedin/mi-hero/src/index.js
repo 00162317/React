d144fa905756a8f83212afd27847304678f38132
Roberto Hernandez <00162317@uca.edu.sv>
2021-04-09 15:52:13 -0600

    Probando nuevo proyecto

--- linkedin/mi-hero/src/index.js
+++ linkedin/mi-hero/src/index.js
@@ -3,9 +3,7 @@ import {render} from 'react-dom'//Para poder mostrar todos los contenidos en mi 
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
-
 import HolaMundo from './components/hola-mundos/HolaMundo';
-
 
 //Llamando a la clase hola mundo
 render(<HolaMundo nombre="Roberto" edad="22"/>,document.querySelector('#root')); 
